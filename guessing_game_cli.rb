decc9ff59f5cd3016bef9509f05a8270eed923c2
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 19:36:29 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def run_guessing_game
+  rand_number = rand(1..6).to_s
+  user_input = gets_user_number
+  
+  case user_input
+  when rand_number
+    puts "You guessed the correct number!"
+  when 'exit'
+    puts "Goodbye!"
+  else
+    print "Sorry! The computer guessed #{rand_number}."
+  end
+end
+
+def gets_user_number
+  gets.chomp
+end
